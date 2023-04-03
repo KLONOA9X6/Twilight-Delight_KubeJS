09c3e961b235afb21c4d8015f793f789a733dddb
KLONOA9X6 <daliu2004@outlook.com>
2023-04-03 18:00:14 +0800

    Update dog_eat_bowl_food.js

--- server_scripts/twilightdelight/events/dog_eat_bowl_food.js
+++ server_scripts/twilightdelight/events/dog_eat_bowl_food.js
@@ -1,9 +1,7 @@
 // 给狼使用处于bowl_food_meat标签内的物品时给予一个碗
-// 淦！找不到一个读取手持物品tag的方法，先放着吧。。。
-//ItemEvents.entityInteracted(event => {
-//    event.server.tell(event.target.type)
-//    event.server.tell(event.player.getHeldItem(event.hand))
-//    if (event.target.type == "minecraft:wolf" && event.player.getHeldItem(event.hand) == '#twilightdelight:bowl_food_meat') {
-//        event.player.giveInHand('minecraft:bowl')
-//    }
-//})
+// 同时判定狼的Age标签是否小于等于0，InLove是否等于0(TODO)
+ItemEvents.entityInteracted(event => {
+    if (event.target.type == "minecraft:wolf" && event.player.getHeldItem(event.hand).hasTag('twilightdelight:bowl_food_meat')) {
+        event.player.giveInHand('minecraft:bowl')
+    }
+})
