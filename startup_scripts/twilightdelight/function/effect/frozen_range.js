b4b5792dc6016efee1a0abcda085a960a6aa4ff9
KLONOA9X6 <daliu2004@outlook.com>
2023-04-04 22:47:16 +0800

    修改火焰范围

--- startup_scripts/twilightdelight/function/effect/frozen_range.js
+++ startup_scripts/twilightdelight/function/effect/frozen_range.js
@@ -1,4 +1,4 @@
 // 霜冻范围间接实现
 global.frozenRange = ctx => {
-    ctx.player.runCommandSilent(`execute at @s as @e[type=!player,distance=..12] run effect give @s twilightforest:frosted 30 4`)
+    ctx.player.runCommandSilent(`execute at @s as @e[distance=..12] run effect give @s twilightforest:frosted 30 4`)
 }
--- startup_scripts/twilightdelight/function/effect/poison_range.js
+++ startup_scripts/twilightdelight/function/effect/poison_range.js
@@ -1,4 +1,4 @@
 // 剧毒范围效果间接实现
 global.poisonRange = ctx => {
-    ctx.player.runCommandSilent(`execute at @s as @e[type=!player,distance=..12] run effect give @s minecraft:poison 1 5`)
+    ctx.player.runCommandSilent(`execute at @s as @e[distance=..12] run effect give @s minecraft:poison 1 5`)
 }
