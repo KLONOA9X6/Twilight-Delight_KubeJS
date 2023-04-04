315cc00a2dee20db5dfabf20c3651fa5a1a5d9d2
KLONOA9X6 <daliu2004@outlook.com>
2023-04-04 14:14:10 +0800

    饮品制作完成，修复无法应用效果到实体上的bug

--- startup_scripts/twilightdelight/function/effect/fire_range.js
+++ startup_scripts/twilightdelight/function/effect/fire_range.js
@@ -1,4 +1,4 @@
 // 火焰范围效果间接实现
 global.fireRange = ctx => {
-    ctx.player.runCommandSilent(`execute at @s as @e[!type=player,distance=..12] run data merge entity @s {Fire:80s}`)
+    ctx.player.runCommandSilent(`execute at @s as @e[type=!player,distance=..12] run data merge entity @s {Fire:80s}`)
 }
--- startup_scripts/twilightdelight/function/effect/temporal_sadness.js
+++ startup_scripts/twilightdelight/function/effect/temporal_sadness.js
@@ -1,7 +1,7 @@
 // 莫名的悲伤效果间接实现
 global.temporalSadness = ctx => {
-    ctx.player.runCommandSilent(`execute at @s as @e[!type=player,distance=..12] run effect give @s minecraft:weakness 30 1`)
-    ctx.player.runCommandSilent(`execute at @s as @e[!type=player,distance=..12] run effect give @s minecraft:slowness 30 1`)
+    ctx.player.runCommandSilent(`execute at @s as @e[type=!player,distance=..12] run effect give @s minecraft:weakness 60 1`)
+    ctx.player.runCommandSilent(`execute at @s as @e[type=!player,distance=..12] run effect give @s minecraft:slowness 60 1`)
     // 粒子效果（TODO）
     ctx.player.tell('你周围的生物忽然感到莫名的悲伤')
 }
