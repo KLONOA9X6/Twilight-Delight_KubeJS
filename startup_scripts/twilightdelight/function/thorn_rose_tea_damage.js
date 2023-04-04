d9c09dd6ee7acaddde8bab96c5658e44f52edbe8
KLONOA9X6 <daliu2004@outlook.com>
2023-04-04 13:38:58 +0800

    修正荆棘玫瑰茶的死亡信息显示bug

--- startup_scripts/twilightdelight/function/thorn_rose_tea_damage.js
+++ startup_scripts/twilightdelight/function/thorn_rose_tea_damage.js
@@ -2,9 +2,17 @@
 global.thornRoseTeaDamage = ctx => {
     ctx.server.runCommandSilent(`gamerule showDeathMessages false`)
     if (ctx.player.getHealth() <= 4) {  // 检测玩家血量是否小于等于4
-        let playerName = ctx.player.getName()
-        ctx.server.tell(`${playerName}承认了每朵玫瑰都有刺`)
+        ctx.player.kill()
+        let playerName = ctx.player.displayName
+        ctx.server.tell([playerName.append(Text.of('')), `承认了每朵玫瑰都有刺`])
+        // 另一种写法，playerName是不可被点击的
+        // let playerName = ctx.player.displayName.getString()
+        // ctx.server.tell(`${playerName}承认了每朵玫瑰都有刺`)
+        // 也可以这样，但是这么搞整句都可以被点击
+        // ctx.server.tell(playerName.append(`承认了每朵玫瑰都有刺`))
     }
-    ctx.player.attack(4)  // 给予玩家4点伤害
-    ctx.server.runCommandSilent(`gamerule showDeathMessages true`)
+    ctx.player.attack(4)
+    ctx.server.schedule(4, (callback) => {
+        callback.server.runCommandSilent(`gamerule showDeathMessages true`)
+    })
 }
